4a32cb14520051686be92e8e00350ff699d6ef0a
chengtao511322 <18088121818@163.com>
2021-11-15 10:16:00 +0800

    纯净版本的kafka-connect-oracle

--- src/main/java/com/ecer/kafka/connect/oracle/OracleSourceConnectorConfig.java
+++ src/main/java/com/ecer/kafka/connect/oracle/OracleSourceConnectorConfig.java
@@ -26,8 +26,9 @@ public class OracleSourceConnectorConfig extends AbstractConfig {
   public static final String TABLE_BLACKLIST = "table.blacklist";
   public static final String DML_TYPES = "dml.types";
   public static final String MAP_UNESCAPED_STRINGS = "map.unescaped.strings";
+  public static final String START_TIME = "start.time";
 
-  
+
   public OracleSourceConnectorConfig(ConfigDef config, Map<String, String> parsedConfig) {
     super(config, parsedConfig);
   }
@@ -38,22 +39,23 @@ public class OracleSourceConnectorConfig extends AbstractConfig {
 
   public static ConfigDef conf() {
     return new ConfigDef()
-        .define(DB_NAME_ALIAS, Type.STRING, Importance.HIGH, "Db Name Alias")
-        .define(TOPIC_CONFIG, Type.STRING, Importance.HIGH, "Topic")
-        .define(DB_NAME_CONFIG, Type.STRING, Importance.HIGH, "Db Name")
-        .define(DB_HOST_NAME_CONFIG,Type.STRING,Importance.HIGH,"Db HostName")
-        .define(DB_PORT_CONFIG,Type.INT,Importance.HIGH,"Db Port")
-        .define(DB_USER_CONFIG,Type.STRING,Importance.HIGH,"Db User")
-        .define(DB_USER_PASSWORD_CONFIG,Type.STRING,Importance.HIGH,"Db User Password")
-        .define(TABLE_WHITELIST,Type.STRING,Importance.HIGH,"TAbles will be mined")
-        .define(PARSE_DML_DATA,Type.BOOLEAN,Importance.HIGH,"Parse DML Data")
-        .define(DB_FETCH_SIZE,Type.INT,Importance.HIGH,"Database Record Fetch Size")
-        .define(RESET_OFFSET,Type.BOOLEAN,Importance.HIGH,"Reset Offset")
-        .define(START_SCN,Type.STRING,"",Importance.LOW,"Start SCN")
-        .define(MULTITENANT, Type.BOOLEAN, Importance.HIGH, "Database is multitenant (container)")
-        .define(TABLE_BLACKLIST, Type.STRING, Importance.LOW, "Table will not be mined")
-        .define(DML_TYPES, Type.STRING, "", Importance.LOW, "Types of DML to capture, CSV value of INSERT/UPDATE/DELETE")
-        .define(MAP_UNESCAPED_STRINGS, Type.BOOLEAN, false, Importance.LOW, "Mapped values for data/before will have unescaped strings");
+            .define(DB_NAME_ALIAS, Type.STRING, Importance.HIGH, "Db Name Alias")
+            .define(TOPIC_CONFIG, Type.STRING, Importance.HIGH, "Topic")
+            .define(DB_NAME_CONFIG, Type.STRING, Importance.HIGH, "Db Name")
+            .define(DB_HOST_NAME_CONFIG,Type.STRING,Importance.HIGH,"Db HostName")
+            .define(DB_PORT_CONFIG,Type.INT,Importance.HIGH,"Db Port")
+            .define(DB_USER_CONFIG,Type.STRING,Importance.HIGH,"Db User")
+            .define(DB_USER_PASSWORD_CONFIG,Type.STRING,Importance.HIGH,"Db User Password")
+            .define(TABLE_WHITELIST,Type.STRING,Importance.HIGH,"TAbles will be mined")
+            .define(PARSE_DML_DATA,Type.BOOLEAN,Importance.HIGH,"Parse DML Data")
+            .define(DB_FETCH_SIZE,Type.INT,Importance.HIGH,"Database Record Fetch Size")
+            .define(RESET_OFFSET,Type.BOOLEAN,Importance.HIGH,"Reset Offset")
+            .define(START_SCN,Type.STRING,"",Importance.LOW,"Start SCN")
+            .define(MULTITENANT, Type.BOOLEAN, Importance.HIGH, "Database is multitenant (container)")
+            .define(TABLE_BLACKLIST, Type.STRING, Importance.LOW, "Table will not be mined")
+            .define(DML_TYPES, Type.STRING, "", Importance.LOW, "Types of DML to capture, CSV value of INSERT/UPDATE/DELETE")
+            .define(MAP_UNESCAPED_STRINGS, Type.BOOLEAN, false, Importance.LOW, "Mapped values for data/before will have unescaped strings")
+            .define(START_TIME,Type.STRING,"",Importance.LOW,"Start logminer Time");
   }
 
   public String getDbNameAlias(){ return this.getString(DB_NAME_ALIAS);}
@@ -72,4 +74,7 @@ public class OracleSourceConnectorConfig extends AbstractConfig {
   public String getTableBlackList(){return this.getString(TABLE_BLACKLIST);}
   public String getDMLTypes(){return this.getString(DML_TYPES);}
   public Boolean getMapUnescapedStrings(){return this.getBoolean(MAP_UNESCAPED_STRINGS);}
+  public String getStartTime(){return this.getString(START_TIME);}
+
+
 }
